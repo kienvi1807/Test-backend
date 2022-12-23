28f5562ef517189ccd49d60641928bab42a65408
kienvi1807 <trungkien2899@gmail.com>
2022-12-23 18:57:49 +0700

    Update files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 5000
-const cors = require('cors')
-
-app.use(cors( ))
+const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('oke')
+    res.json({success: true})
 })
 
 app.listen(port, () => {
-    console.log(`App listening http://localhost:${port}`)
+    console.log('oke')
 })
