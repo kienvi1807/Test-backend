95fed107ca18b3dc1d6222eb2fcd12df943174c8
kienvi1807 <trungkien2899@gmail.com>
2022-12-23 20:47:30 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const cors = require('cors')
-
 app.use(cors())
 
 app.get('/', (req, res) => {
